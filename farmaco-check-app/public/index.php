9ff0640d7dce905e5be4d0c95949db597aa42226
Silvio Martins <silviosmsantos@gmail.com>
2024-12-14 17:18:22 -0300

    env: restore code index.php

--- farmaco-check-app/public/index.php
+++ farmaco-check-app/public/index.php
@@ -4,28 +4,14 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-try {
-    // Define headers de segurança
-    header('X-Content-Type-Options: nosniff');
-    header('X-Frame-Options: DENY');
-    header('X-XSS-Protection: 1; mode=block');
-
-    // Verifica se o app está em modo de manutenção
-    $maintenance = __DIR__.'/../storage/framework/maintenance.php';
-    if (is_readable($maintenance)) {
-        require $maintenance;
-    }
-
-    // Registra o autoloader do Composer
-    require realpath(__DIR__.'/../vendor/autoload.php');
-
-    // Bootstrap da aplicação Laravel e tratamento da request
-    (require_once realpath(__DIR__.'/../bootstrap/app.php'))
-        ->handleRequest(Request::capture());
-} catch (Throwable $e) {
-    // Tratamento de erros
-    error_log($e->getMessage());
-    http_response_code(500);
-    echo "Ocorreu um erro. Por favor, tente novamente mais tarde.";
-    exit(1);
+// Determine if the application is in maintenance mode...
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
 }
+
+// Register the Composer autoloader...
+require __DIR__.'/../vendor/autoload.php';
+
+// Bootstrap Laravel and handle the request...
+(require_once __DIR__.'/../bootstrap/app.php')
+    ->handleRequest(Request::capture());
