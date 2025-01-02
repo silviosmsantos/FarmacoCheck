db1d1814143889e54d75c0980c2441ffa9ddd833
Silvio Martins <silviosmsantos@gmail.com>
2025-01-01 22:36:41 -0300

    feature: implementing manager users and add livewire powergrid

--- farmaco-check-app/routes/web.php
+++ farmaco-check-app/routes/web.php
@@ -1,15 +1,14 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+Route::view('dashboard', 'dashboard')->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::view('profile', 'profile')
-    ->middleware(['auth'])
-    ->name('profile');
+Route::view('profile', 'profile')->middleware(['auth'])->name('profile');
+
+Route::get('/users', [UserController::class , 'index'])->middleware(['auth', 'verified' , 'can:view users'])->name('users');
 
 require __DIR__.'/auth.php';
--- farmaco-check-app/tailwind.config.js
+++ farmaco-check-app/tailwind.config.js
@@ -1,6 +1,8 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
-const colors = require('tailwindcss/colors')
+
+const colors = require('tailwindcss/colors');
+const powergridConfig = require('./vendor/power-components/livewire-powergrid/tailwind.config.js');
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -12,19 +14,24 @@ export default {
         "./vendor/wireui/wireui/ts/**/*.ts",
         "./vendor/wireui/wireui/src/WireUi/**/*.php",
         "./vendor/wireui/wireui/src/Components/**/*.php",
+        './app/Livewire/**/*Table.php',
+        './vendor/power-components/livewire-powergrid/resources/views/**/*.php',
+        './vendor/power-components/livewire-powergrid/src/Themes/Tailwind.php'
     ],
 
     theme: {
         extend: {
             colors: {
                 primary: colors.emerald,
-                secondary: colors.blue
+                secondary: colors.blue,
+                ...powergridConfig.theme?.colors // Certifique-se de que o Powergrid possui um objeto colors
             },
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
         },
     },
+
     presets: [
         require("./vendor/wireui/wireui/tailwind.config.js")
     ],
